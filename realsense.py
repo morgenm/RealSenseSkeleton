1c3f86798aea3224b41db0f15d2be52bbce5f184
Morgen Malinoski <morgenem@protonmail.com>
2021-12-17 02:49:47 +0000

    Added preloading frames with threading and a queue.

--- realsense.py
+++ realsense.py
@@ -11,6 +11,8 @@ import re
 import time, datetime
 import render, record
 import settings
+from multiprocessing import Process
+import queue
 
 def IsDirEmpty(dir):
     if not os.path.isdir(dir):
@@ -33,6 +35,55 @@ def GetValidNewDirFromFilename(fullPath):
         sub += 1
     return basename
 
+class PlaybackFrame:
+    def __init__(self, color_image, skeletons, depth, depth_intrinsic):
+        self.color_image = color_image
+        self.skeletons = skeletons
+        self.depth = depth
+        self.depth_intrinsic = depth_intrinsic
+
+def GetPickledAtFrame(pickleFile, frame_number):
+    # Starting at next pickle, loop until its found
+    found = False
+    p = None
+    while not found:
+        try:
+            p = pickle.load(pickleFile)
+        except EOFError:
+            return None
+        
+        if p[0] == frame_number:
+            found = True
+    
+    if not found:
+        return None
+    else:
+        return p[1]
+
+def LoadPlaybackData(in_queue, out_frames):
+    while True:
+        frame_number, image_loc, skeletons_file, depth_file, intrinsic_file = in_queue.get()
+        
+        if frame_number == "Quit": # Quit signal
+            in_queue.task_done()
+            break
+        
+        # Get color image
+        color_image = cv2.imread(image_loc)
+        
+        # Get skeletons
+        skeletons = GetPickledAtFrame(skeletons_file, frame_number)
+        
+        # Get depth
+        depth = GetPickledAtFrame(depth_file, frame_number)
+        
+        # Get depth_intrinsic
+        depth_intrinsic = GetPickledAtFrame(intrinsic_file, frame_number)
+        
+        # Return frame
+        out_frames[frame_number] = PlaybackFrame(color_image, skeletons, depth, depth_intrinsic)
+        
+        in_queue.task_done()
 
 def MainProgram(args):
     # Check mode
@@ -129,15 +180,43 @@ def MainProgram(args):
                 def human_key(text):
                     return [atoi(c) for c in re.split(r'(\d+)', text)]
                 
-                # Load images and depth data
+                # Open files
                 imageDir = os.path.join(workDir, "image/")
                 sortedImages = os.listdir(imageDir)
                 sortedImages.sort(key=human_key)
-                color_images = {}
+                skels = open(os.path.join(workDir, "save.skel"), "rb")
+                depth_file = open(os.path.join(workDir, "depth.sav"), 'rb')
+                intrinsic_file = open(os.path.join(workDir, "depth_intrinsic.sav"), 'rb')
+                
+                # Load frames with multiprocessing
+                loaded_frames = {}
+                load_queue = queue.Queue()
+                last_queued_frame_number = 0
+                total_frames = len(sortedImages)
+                LOAD_AHEAD = 3
+                playback_done = False
+                
+                # Preload
+                while last_queued_frame_number < LOAD_AHEAD:
+                    load_queue.put((last_queued_frame_number, 
+                                    os.path.join(imageDir, sortedImages[last_queued_frame_number]),
+                                    skels, depth_file, intrinsic_file))
+                    last_queued_frame_number += 1
+                
+                import threading
+                p = threading.Thread(target=LoadPlaybackData, args=(load_queue, loaded_frames))
+                p.start()
+                
+                # Wait til loading min preload frames
+                while len(loaded_frames) < LOAD_AHEAD:
+                    print("Wai                playback_done = Trueting {}".format(len(loaded_frames)))
+                    time.sleep(1)
+                
+                '''color_images = {}
                 skeletons_frames = {}
                 depth_frames = {}
-                depth_intrinsic_frames = {}
-                for imageLoc in sortedImages:
+                depth_intrinsic_frames = {}'''
+                '''for imageLoc in sortedImages:
                     imageCounter = human_key(imageLoc)[1]
                     color_images[imageCounter] = cv2.imread(os.path.join(imageDir, imageLoc))
                     
@@ -196,15 +275,77 @@ def MainProgram(args):
                     if depth_intrinsic == None:
                         print("No depth intrinsic found for frame: {}".format(imageCounter))
                     else:
-                        depth_intrinsic_frames[imageCounter] = depth_intrinsic
+                        depth_intrinsic_frames[imageCounter] = depth_intrinsic'''
                         
                     
                 # Render each frame
                 print("Playing...")
                 cv2.namedWindow(settings.window_name_playback, cv2.WINDOW_NORMAL + cv2.WINDOW_KEEPRATIO) # Create Window
                 last_time = datetime.datetime.now() # Last render time
-                for frame_number in color_images:
-                    if frame_number in color_images and frame_number in skeletons_frames and frame_number in depth_frames and frame_number in depth_intrinsic_frames:
+                #for frame_number in color_images:
+                print("Total frames: ", total_frames)
+                for frame_number in range(total_frames):
+                    while frame_number not in loaded_frames:
+                        print("Frame {} not loaded yet...".format(frame_number))
+                    
+                    # Frame loaded, play now
+                    frame = loaded_frames[frame_number]
+                    color_image = frame.color_image
+                    skeletons = frame.skeletons
+                    depth = frame.depth
+                    depth_intrinsic = frame.depth_intrinsic
+                    
+                    # Verify all data is here
+                    play = True
+                    if color_image is None:
+                        print("No image for frame {}".format(frame_number))
+                        play = False
+                    if skeletons is None:
+                        print("No skeletons for frame {}".format(frame_number))
+                        play = False
+                    if depth is None:
+                        print("No depth for frame {}".format(frame_number))
+                        play = False
+                    if depth_intrinsic is None:
+                        print("No depth_intrinsic for frame {}".format(frame_number))
+                        play = False
+                    
+                    if play:
+                        # render the skeletons on top of the acquired image and display it
+                        cm.render_result(skeletons, color_image, joint_confidence)
+            
+                        render.render_ids_3d(
+                            color_image, skeletons, depth, depth_intrinsic, joint_confidence
+                        )
+                        cv2.imshow(settings.window_name_playback, color_image)
+                        
+                        if cv2.waitKey(1) == 27:
+                            break
+                    else:
+                        print("Could not play.")
+                    
+                    del loaded_frames[frame_number]
+                    while load_queue.qsize() < min(LOAD_AHEAD, total_frames - last_queued_frame_number ):
+                        load_queue.put((last_queued_frame_number, 
+                                    os.path.join(imageDir, sortedImages[last_queued_frame_number]),
+                                    skels, depth_file, intrinsic_file))
+                        last_queued_frame_number += 1
+                    
+                    # Get delta time and wait if necessary
+                    frame_str = "{}".format(frame_number)
+                    if frame_str in frame_data:
+                        now = datetime.datetime.now()
+                        delta = now - last_time
+                        delta = delta.total_seconds()
+                        last_time = now
+                        
+                        record_delta = frame_data[frame_str]["Delta Time"]
+                        if record_delta > delta:
+                            time.sleep(record_delta - delta)
+                    else:
+                        print("No frame data found for frame: {}".format(frame_number))
+                    
+                    '''if frame_number in color_images and frame_number in skeletons_frames and frame_number in depth_frames and frame_number in depth_intrinsic_frames:
                         color_image = color_images[frame_number]
                         skeletons = skeletons_frames[frame_number]
                         depth = depth_frames[frame_number]
@@ -233,9 +374,18 @@ def MainProgram(args):
                         if record_delta > delta:
                             time.sleep(record_delta - delta)
                     else:
-                        print("No frame data found for frame: {}".format(frame_number))
-            
+                        print("No frame data found for frame: {}".format(frame_number))'''
+
+                load_queue.put(("Quit", None, None, None, None))
+                load_queue.join()
+                print("Waiting on thread...")
+                p.join()
                 cv2.destroyAllWindows()
+                
+                # Close data files
+                skels.close()
+                depth_file.close()
+                intrinsic_file.close()
                 
                 # Delete save dir
                 shutil.rmtree(workDir, ignore_errors=True)
--- settings.py
+++ settings.py
@@ -7,3 +7,5 @@ Settings
 window_name_playback = "RealSense Skeleton Tracking Playback"
 data_compress_level = 6
 data_file_extension = ".tar.gz"
+frames_per_second = 24
+load_ahead_seconds = 3 # How many seconds of video to load ahead during playback
